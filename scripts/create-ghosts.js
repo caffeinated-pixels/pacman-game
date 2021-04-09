5cb6fc4d241b04488e5c3d4d6e296c598f672d36
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-04-09 13:49:29 -0400

    refactored ghost movement

--- scripts/create-ghosts.js
+++ scripts/create-ghosts.js
@@ -1,20 +1,22 @@
 class Ghost {
-  constructor (className, startIndex, speed) {
+  constructor (className, startIndex, speed, direction) {
     this.className = className
     this.startIndex = startIndex
     this.speed = speed
     this.currentIndex = startIndex
+    this.currentDirection = direction
+    this.newIndex = startIndex + direction
     this.isScared = false
     this.timerId = NaN
   }
 }
 
-export function createNewGhosts () {
+export function createNewGhosts (width) {
   return [
-    new Ghost('blinky', 348, 250),
-    new Ghost('pinky', 376, 400),
-    new Ghost('inky', 351, 300),
-    new Ghost('clyde', 379, 500)
+    new Ghost('blinky', 349, 250, -width),
+    new Ghost('pinky', 405, 400, -width),
+    new Ghost('inky', 350, 300, -width),
+    new Ghost('clyde', 406, 500, -width)
   ]
 }
 
