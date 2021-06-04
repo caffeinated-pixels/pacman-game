baed21e9089e5746371659e1a49dd0169ea30b35
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-06-04 07:28:40 -0400

    destructure ghost factory function

--- scripts/create-ghosts.js
+++ scripts/create-ghosts.js
@@ -1,50 +1,21 @@
-// class Ghost {
-//   constructor (
-//     className,
-//     startIndex,
-//     respawnIndex,
-//     startTimer,
-//     speed,
-//     direction,
-//     scatterTarget
-//   ) {
-//     this.className = className
-//     this.startIndex = startIndex
-//     this.respawnIndex = respawnIndex
-//     this.startTimer = startTimer // state.dotsEaten threshold
-//     this.speed = speed
-//     this.currentIndex = startIndex
-//     this.currentDirection = direction
-//     this.scatterTarget = scatterTarget
-//     this.targetTile = 0
-//     this.nextDirection = 0
-//     this.isFrightened = false
-//     this.isFlashing = false
-//     this.firstMoveAfterFrightened = false
-//     this.timerId = NaN
-//     this.frightenedTimer = NaN
-//     this.flashTimerId = NaN
-//   }
-// }
-
 function ghost (
   className,
   startIndex,
   respawnIndex,
   startTimer,
   speed,
-  direction,
+  currentDirection,
   scatterTarget
 ) {
   return {
-    className: className,
-    startIndex: startIndex,
-    respawnIndex: respawnIndex,
-    startTimer: startTimer, // state.dotsEaten threshold
-    speed: speed,
+    className,
+    startIndex,
+    respawnIndex,
+    startTimer, // state.dotsEaten threshold
+    speed,
+    currentDirection,
+    scatterTarget,
     currentIndex: startIndex,
-    currentDirection: direction,
-    scatterTarget: scatterTarget,
     targetTile: 0,
     nextDirection: 0,
     isFrightened: false,
@@ -55,20 +26,6 @@ function ghost (
     flashTimerId: NaN
   }
 }
-
-// export function createNewGhosts (width) {
-//   return [
-//     new Ghost('blinky', 321, 377, 0, 500, -1, -1),
-//     new Ghost('pinky', 378, 378, 5, 500, -width, -28),
-//     new Ghost('inky', 376, 376, 30, 500, 1, 895),
-//     new Ghost('clyde', 379, 379, 90, 500, -1, 868)
-//   ]
-//
-//   // return [new Ghost('clyde', 379, 379, 0, 500, -1, 868)]
-//   // return [new Ghost('blinky', 321, 377, 0, 1000, -1, -1)]
-//   // return [new Ghost('blinky', 394, 377, 0, 1000, -1, -1)]
-//   // return [new Ghost('blinky', 417, 377, 0, 1000, 1, -1)]
-// }
 
 export function createNewGhosts (width) {
   return [
