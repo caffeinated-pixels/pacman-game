58006802364829a2eeb231395277c666dbf5e214
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2021-06-04 07:13:46 -0400

    convert ghost class into factory function

--- scripts/create-ghosts.js
+++ scripts/create-ghosts.js
@@ -1,38 +1,81 @@
-class Ghost {
-  constructor (
-    className,
-    startIndex,
-    respawnIndex,
-    startTimer,
-    speed,
-    direction,
-    scatterTarget
-  ) {
-    this.className = className
-    this.startIndex = startIndex
-    this.respawnIndex = respawnIndex
-    this.startTimer = startTimer // state.dotsEaten threshold
-    this.speed = speed
-    this.currentIndex = startIndex
-    this.currentDirection = direction
-    this.scatterTarget = scatterTarget
-    this.targetTile = 0
-    this.nextDirection = 0
-    this.isFrightened = false
-    this.isFlashing = false
-    this.firstMoveAfterFrightened = false
-    this.timerId = NaN
-    this.frightenedTimer = NaN
-    this.flashTimerId = NaN
+// class Ghost {
+//   constructor (
+//     className,
+//     startIndex,
+//     respawnIndex,
+//     startTimer,
+//     speed,
+//     direction,
+//     scatterTarget
+//   ) {
+//     this.className = className
+//     this.startIndex = startIndex
+//     this.respawnIndex = respawnIndex
+//     this.startTimer = startTimer // state.dotsEaten threshold
+//     this.speed = speed
+//     this.currentIndex = startIndex
+//     this.currentDirection = direction
+//     this.scatterTarget = scatterTarget
+//     this.targetTile = 0
+//     this.nextDirection = 0
+//     this.isFrightened = false
+//     this.isFlashing = false
+//     this.firstMoveAfterFrightened = false
+//     this.timerId = NaN
+//     this.frightenedTimer = NaN
+//     this.flashTimerId = NaN
+//   }
+// }
+
+function ghost (
+  className,
+  startIndex,
+  respawnIndex,
+  startTimer,
+  speed,
+  direction,
+  scatterTarget
+) {
+  return {
+    className: className,
+    startIndex: startIndex,
+    respawnIndex: respawnIndex,
+    startTimer: startTimer, // state.dotsEaten threshold
+    speed: speed,
+    currentIndex: startIndex,
+    currentDirection: direction,
+    scatterTarget: scatterTarget,
+    targetTile: 0,
+    nextDirection: 0,
+    isFrightened: false,
+    isFlashing: false,
+    firstMoveAfterFrightened: false,
+    timerId: NaN,
+    frightenedTimer: NaN,
+    flashTimerId: NaN
   }
 }
 
+// export function createNewGhosts (width) {
+//   return [
+//     new Ghost('blinky', 321, 377, 0, 500, -1, -1),
+//     new Ghost('pinky', 378, 378, 5, 500, -width, -28),
+//     new Ghost('inky', 376, 376, 30, 500, 1, 895),
+//     new Ghost('clyde', 379, 379, 90, 500, -1, 868)
+//   ]
+//
+//   // return [new Ghost('clyde', 379, 379, 0, 500, -1, 868)]
+//   // return [new Ghost('blinky', 321, 377, 0, 1000, -1, -1)]
+//   // return [new Ghost('blinky', 394, 377, 0, 1000, -1, -1)]
+//   // return [new Ghost('blinky', 417, 377, 0, 1000, 1, -1)]
+// }
+
 export function createNewGhosts (width) {
   return [
-    new Ghost('blinky', 321, 377, 0, 500, -1, -1),
-    new Ghost('pinky', 378, 378, 5, 500, -width, -28),
-    new Ghost('inky', 376, 376, 30, 500, 1, 895),
-    new Ghost('clyde', 379, 379, 90, 500, -1, 868)
+    ghost('blinky', 321, 377, 0, 500, -1, -1),
+    ghost('pinky', 378, 378, 5, 500, -width, -28),
+    ghost('inky', 376, 376, 30, 500, 1, 895),
+    ghost('clyde', 379, 379, 90, 500, -1, 868)
   ]
 
   // return [new Ghost('clyde', 379, 379, 0, 500, -1, 868)]
