97e84ceed59e7f1682b4607f342d1bb73ed6e659
Stevie Gill <29311273+caffeinated-pixels@users.noreply.github.com>
2023-12-03 09:05:36 -0500

    fix: blank board on next level

--- src/app/functions/levelChecks.ts
+++ src/app/functions/levelChecks.ts
@@ -2,7 +2,7 @@ import { stopPowerPillSound } from '../constants/audioObjects'
 import { grid } from '../constants/dom'
 import { createBoard } from './createBoard'
 import { removeAllGhosts, removePacman } from './gameOver'
-import { GameState } from './initializeState'
+import { GameState, createSquares } from './initializeState'
 import { getReadyTimer } from './stopStartGame'
 
 export const checkForLevelEnd = (state: GameState) => {
@@ -13,7 +13,7 @@ export const checkForLevelEnd = (state: GameState) => {
   }
 }
 
-function startNextLevel(state: GameState) {
+const startNextLevel = (state: GameState) => {
   state.isPaused = true
   state.dotsEaten = 0
   state.ghostsEatenPoints = 200
@@ -25,6 +25,7 @@ function startNextLevel(state: GameState) {
   removePacman(state, pacmanCurrentTile)
 
   grid.innerHTML = ''
+  state.squares = createSquares()
   createBoard(state)
 
   getReadyTimer(state)
